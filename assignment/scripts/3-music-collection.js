1757120fa3fe7829557af7a6a6e552d2afb99f59
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-02-15 13:52:45 -0600

    added addToCollection function and testing

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,12 @@
 console.log('***** Music Collection *****')
 let collection = [];
+function addToCollection(title, artist, yearPublished) {
+  let title = {
+    title: title;
+    artist: artist;
+    yearPublished: yearPublished;
+  }
+  collection.push(title)
+}
+console.log(addToCollection('September', 'Earth, Wind & Fire', '1978'))
+console.log(collection);
