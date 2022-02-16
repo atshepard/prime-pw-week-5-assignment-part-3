ae3d5d30b09377fc4fa68bc2c3fad0ff338c840f
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-02-16 14:15:14 -0600

    added and tested findByArtist function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -17,11 +17,33 @@ console.log(addToCollection('Discovery', 'Daft Punk', '2001'));
 console.log(addToCollection('Paul Simon', 'Paul Simon', '1972'));
 console.log(collection);
 
+
+//This worked great, but I wanted to try something else as well:
+// function showCollection(array) {
+//   console.log(array.length);
+//     for (let i = 0; i < array.length; i++) {
+//       console.log(array[i].title, 'by', array[i].artist, ', published in', array[i].yearPublished);
+//     }
+//   }
+
 function showCollection(array) {
   console.log(array.length);
-    for (let i = 0; i < array.length; i++) {
-      console.log(array[i].title, 'by', array[i].artist, ', published in', array[i].yearPublished);
-    }
+    array.forEach((album) => {
+      console.log(album.title, 'by', album.artist, ', published in', album.yearPublished);
+    });
   }
 
 showCollection(collection);
+
+function findByArtist (artist) {
+  let findArtistResults = [];
+    for (let i = 0; i < collection.length; i++) {
+          if (collection[i].artist === artist) {
+            findArtistResults.push(collection[i]);
+          }
+        }
+  return findArtistResults;
+      }
+
+console.log(findByArtist('The Black Keys'));
+console.log(findByArtist('The BeeGees'));
