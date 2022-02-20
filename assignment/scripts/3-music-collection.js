5f974c8b17618c160af45700a818d3b9e1dc7706
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-02-20 12:55:50 -0600

    researching and testing search function and nested arrays for tracklist

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,21 +12,23 @@ console.log('***** Music Collection *****')
 //   return newAlbum;
 // }
 
+//As is: only allows to add 1 track to tracklist. Still looking into the correct way.
 let collection = []; //empty array to house collection
 function addToCollection(title, artist, yearPublished, tracks) {
+  let trackList = []
+    trackList.push(tracks)
   let newAlbum = {
     title: title,
     artist: artist,
     yearPublished: yearPublished,
     //tracks: [{trackName: trackName, trackDuration: trackDuration}] -> gives 'trackname not defined'
-    tracks: [], //trying an empty array that I can pass multiple objects into...
+    albumTracks: trackList //trying an empty array that I can pass multiple objects into...
   } //end newAlbum object
   collection.push(newAlbum); //adds the new album to the end of the collection array.
   return newAlbum; //returns newAlbum
 }
-console.log('adding album', addToCollection('September', 'Earth, Wind & Fire', '1978',
-            [{trackName: 'September', trackDuration: '3:35'}]));
-console.log('adding album', addToCollection('El Camino', 'The Black Keys', '2010')); //was actually released in 2011, but changed to test search function.
+console.log('adding album', addToCollection('September', 'Earth, Wind & Fire', '1978', {title: 'September', duration: '3:35' }));
+console.log('adding album', addToCollection('El Camino', 'The Black Keys', '2010', {title: 'Lonely Boy', duration: '3:13'} )); //was actually released in 2011, but changed to test search function.
 console.log('adding album', addToCollection('Magic Potion', 'The Black Keys', '2006'));
 console.log('adding album', addToCollection('Brothers', 'The Black Keys', '2010'));
 console.log('adding album', addToCollection('Discovery', 'Daft Punk', '2001'));
@@ -61,14 +63,15 @@ function findByArtist (artist) {
   return findArtistResults;
       }
 
-console.log(findByArtist('The Black Keys'));
-console.log(findByArtist('The BeeGees'));
+console.log('Searching for artist, The Black Keys:', findByArtist('The Black Keys'));
+console.log('Searching for artist, The BeeGees', findByArtist('The BeeGees'));
 
-//Initial thoughts on search function, but doesn't *exactly* meet the assignment parameters:
+//Initial thoughts on search function, but doesn't *exactly* meet the assignment parameters, because the Logical OR operator doesn't match ALL the search criteria
+
 // function search(input) {
 //   let searchResults = [];
 //     for (let i = 0; i < collection.length; i++) {
-//       if (collection[i].artist === input || collection[i].title === input || collection[i].yearPublished == input) {
+//       if (collection[i].artist === input.artist || collection[i].title === input.title || collection[i].yearPublished == input.yearPublished) {
 //         searchResults.push(collection[i]);
 //       } else if (input === "" || !input) {
 //         return collection;
@@ -80,19 +83,16 @@ console.log(findByArtist('The BeeGees'));
 function search(array, input) { //trying to see if we can pass collection into the function as well, so the search can be used across collections.
   let searchResults = [];
     for (let i = 0; i < array.length; i++) {
-      if (array[i].artist === input.artist && array[i].title === input.title && array[i].yearPublished == input.yearPublished) {
-        searchResults.push(array[i]);
-      } else if (input === "" || !input) { //if input is empty or no input is provided, it will return the entire collection passed through the function.
+      if (input == "" || !input) { //first checking if there is an input or not
         return array;
+      } else if (array[i].artist == input.artist && array[i].title == input.title && array[i].yearPublished == input.yearPublished) { //then comparing input values against search collection
+        searchResults.push(array[i]);
           }
         }
   return searchResults;
 }
 
-console.log(search(collection, 'September'));
-console.log(search(collection, 'Paul Simon')); //should only return one instance of the album since the conditional is 'or', even though title and artist match.
-console.log(search(collection, '2010')); //testing to make sure it shows two Black Keys albums
-console.log(search(collection, 2010)); //should still return two Black Keys albums
-console.log(search(collection, []));
+console.log('Searching for September:', search(collection, {title: 'September', artist: 'Earth, Wind & Fire', yearPublished: 1978}));
+console.log('Searching for Paul Simon:', search(collection, {title: 'Paul Simon', artist: 'Paul Simon', yearPublished: 1972}));
 console.log(search(collection, ''));
-console.log(search(collection));
+console.log(search(collection, ));
